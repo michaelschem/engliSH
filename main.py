78958637c287cb5d572621909f8ec14eb68057ad
mschem <mike.schem@stringking.com>
2023-02-10 20:44:44 -0800

    working well, only completes on tab, not while typing

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import Completer, Completion
-import openai
 import os
+import asyncio
+import time
+import openai
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import Completer, Completion
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-def get_command(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0,
-    )
-    message = response["choices"][0]["text"]
-    return message.strip('\n')
-
-class OpenAICompleter(Completer):
+class GPTCompleter(Completer):
     def get_completions(self, document, complete_event):
+        # print(self, document, complete_event)
+
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=f"Give me some mac bash that will {document.text}",
+            prompt=f"Give me some mac bash that will {document.text}. Only provide the code, not the prompt.",
             max_tokens=1024,
-            n=10,
+            n=3,
             stop=None,
             temperature=0.5,
         )
-        options = [choice["text"] for choice in response["choices"]]
-        for option in options:
-            yield Completion(option, start_position=-len(document.text))
-
-completer = OpenAICompleter()
+        options = set([choice["text"].replace('\n', '') for choice in response["choices"]])
+        options = [Completion(option, start_position=-len(document.text)) for option in options]
+        # yield Completion('completion3', start_position=-5)
+        return options
 
 while True:
-    request = prompt("gpt_bash: ", completer=completer)
-    if request == "quit":
-        break
-    bash_command = get_command(f"Give me some mac bash that will {request}")
-    print(f"Bash Command: {bash_command}")
+    session = PromptSession(completer=GPTCompleter(), complete_while_typing=False)
+    result = session.prompt('> ')
+    # print(result)
+    os.system(result)
